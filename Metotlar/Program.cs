3f4d9308ae3d500c87039a54b5afc85958573258
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-02-13 00:18:40 +0300

    Metotlar projesi tamamlandi.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int a = 2;
+int b = 3;
+
+Console.WriteLine(a+b);
+
+int sonuc = Topla(5, 89);
+EkranaYazdir(Convert.ToString(sonuc));
+
+static int Topla(int sayi1, int sayi2)
+{
+    return sayi1+sayi2;
+}
+
+static void EkranaYazdir(string veri)
+{
+    Console.WriteLine(veri);
+}
