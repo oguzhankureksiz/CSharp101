5d4908404caf27ab4009958c17fe5799cadad8af
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-02-12 11:18:28 +0300

    SwitchCase projesi tamamlandi.

---
+++ KosulluYapilar2/Program.cs
@@ -0,0 +1,43 @@
+﻿int month = DateTime.Now.Month;
+
+switch (month)
+{
+    case 1:
+        Console.WriteLine("Ocak ayındasınız!");
+        break;
+    case 2:
+        Console.WriteLine("Şubat ayındasınız!");
+        break;
+    case 3:
+        Console.WriteLine("Mart ayındasınız!");
+        break;
+    default:
+        Console.WriteLine("Yanlış veri girdiniz!");
+        break;
+}
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış ayındasınız!");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("İlkbahar ayındasınız!");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Yaz ayındasınız!");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Sonabahar ayındasınız!");
+        break;
+    default:
+        break;
+}
