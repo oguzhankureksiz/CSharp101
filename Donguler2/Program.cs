0cb64a92063755d12b6f4c0219bf40c4e8d8c767
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-02-12 13:26:01 +0300

    Donguler2 projesi tamamlandi.

---
+++ Donguler2/Program.cs
@@ -0,0 +1,35 @@
+﻿// While
+// 1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program
+
+Console.WriteLine("Bir sayı giriniz : ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+int sayac = 1;
+int toplam = 0;
+while (sayac <= sayi)
+{
+    toplam += sayac;
+    sayac++;
+}
+Console.WriteLine("Ortalama : " + toplam / sayi);
+
+
+
+// 'a' dan 'z' ye kadar olan tüm harfleri ekrana yazdır.
+char ch = 'a';
+while (ch <= 'z')
+{
+    Console.Write(ch + " ");
+    ch++;
+}
+
+
+
+
+Console.WriteLine("***** Foreach *****");
+string[] arabalar = { "Bmw","Ford","Fiesta","Nissan" };
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
