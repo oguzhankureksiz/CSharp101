ff6ba670fcb43b4c67b25c3571e82515132a261c
Oğuzhan Küreksiz <kureksizoguzhan@gmail.com>
2024-02-12 10:55:57 +0300

    KosulluYapilar projesi tamamnlandi.

---
+++ KosulluYapilar/Program.cs
@@ -0,0 +1,18 @@
+﻿int time = DateTime.Now.Hour;
+
+if(time >= 6 && time < 11)
+{
+    Console.WriteLine("Günaydın!");
+}
+else if(time <= 18)
+{
+    Console.WriteLine("İyi günler!");
+}
+else
+{
+    Console.WriteLine("İyi geceler!");
+}
+
+string sonuc = time < 18 ? "İyi Günler!" : "İyi Geceler!";
+sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "İyi GÜnler!" : "İyi Geceler!";
+Console.WriteLine(sonuc);
